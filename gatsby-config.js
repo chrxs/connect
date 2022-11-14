90c7d01a1afeadea712e7ea25934bc4600363ec2
Chris Miller (Chrxs) <pirate.chrxs@gmail.com>
2022-11-14 16:48:14 -0500

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,21 @@
+const path = require("path");
+
 module.exports = {
   siteMetadata: {
     title: `connect`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
-    "gatsby-theme-connect-core"
+    "gatsby-theme-connect-core",
+    {
+      resolve: "gatsby-plugin-root-import",
+      options: {
+        resolveModules: [
+          path.join(__dirname, "src"),
+          path.join(__dirname, "node_modules", "gatsby-theme-connect-core", "src"),
+          path.join(__dirname, "../", "gatsby-theme-connect-core", "src"),
+        ],
+      },
+    },
   ],
 };
--- src/pages/news.js
+++ src/pages/news.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Button } from "theme-ui";
 
 const NewsPage = () => {
   return (
